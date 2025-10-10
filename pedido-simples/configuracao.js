a3a643d5370a07b4fec6c096fde50c8a99011f53
Guilherme Bianchini <guilhermeabianchini@gmail.com>
2025-10-10 19:35:49 -0300

    Initial commit

---
+++ pedido-simples/configuracao.js
@@ -0,0 +1,17 @@
+class Configuracao {
+    constructor() {
+        if(Configuracao.instancia) {
+            return Configuracao.instancia
+        }
+
+        this.moeda = "R$"
+        this.percentualDescontoPadrao = 0.05
+        Configuracao.instancia = this
+    }
+
+    alterarMoeda(novaMoeda) {
+        this.moeda = novaMoeda
+    }
+}
+
+module.exports = new Configuracao
